2875d6bb20e79a79a0f1937c4d46d3b475343eab
dQuixote <rudyardrichter@uchicago.edu>
2015-06-04 22:03:52 -0500

    cleaning up Main

--- src/exc/Main.hs
+++ src/exc/Main.hs
@@ -1,15 +1,9 @@
 module Main (main) where
 
 import Crawler
-import Database
 import Search
 
-import Control.Exception
-import Control.Monad
-import Database.Redis
 import System.Environment
-import System.Exit
-import System.IO
 
 main :: IO ()
 main = do
@@ -24,4 +18,3 @@ usage :: IO ()
 usage = do
     progName <- getProgName
     putStrLn $ "usage: " ++ progName ++ " --crawl | --search"
-    exitWith $ ExitFailure 1
